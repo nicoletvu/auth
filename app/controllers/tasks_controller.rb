f2aee764a4b8c546bd4f6e1a6901a4dd6b5dbb43
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-05-24 02:41:34 +0000

    Add authorization to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,19 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({"user_id" => session["user_id"]})
     @task = Task.new
   end
 
   def create
     @task = Task.new
     @task["description"] = params["task"]["description"]
+    @task["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
 
   def destroy
-    @task = Task.find_by({ "id" => params["id"] })
+    @task = Task.find_by({ "id" => params["id"], "user_id" => session["user_id"]})
     @task.destroy
     redirect_to "/tasks"
   end
