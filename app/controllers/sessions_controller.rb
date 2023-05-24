53fb9d740332b94f8d150acf0ca0c096bbded7d6
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-05-24 02:28:47 +0000

    Authorization basics

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,10 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     if @user != nil  
     # 3. if they know their password -> login is successful
-      if BCrypt::Password.new(@user["password"]) == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"] 
+        # cookies["monster"] = "me like cookies"
+        # cookies["user_id"] = @user["id"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome!"
         redirect_to "/companies"
     # 4. if the email does not exist or they do not know their password -> login fails
@@ -25,7 +28,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # logout the user
+    # logout the user and delete the user id cookie
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/sessions/new"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources "tasks"
   resources "users"
   resources "sessions"
-
+  # we want a log-out route; give me a route that's logout where the controller is sessions and the action is destroy
+  # identifies which controller and action should process the code; goes to sessions/destroy
+  get "/logout", :controller => "sessions", :action => "destroy"
   # get "/", :controller => "sessions", :action => "new"
 end
